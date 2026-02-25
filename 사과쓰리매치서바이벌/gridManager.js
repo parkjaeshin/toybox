e61fdfc58195f0eabbaebb0f445563b95376fe16
parkjaeshin <88084641+parkjaeshin@users.noreply.github.com>
2026-02-25 12:57:17 +0900

    안티그래비티랑 같이 쓰는 중인데
    modified 표시 지울려고 일단 푸시

---
+++ 사과쓰리매치서바이벌/gridManager.js
@@ -0,0 +1,12 @@
+const GRID_WIDTH = 8;
+const GRID_HEIGHT = 8;
+
+const SIZE = 7;
+const objArr =
+    Array.from({ length: SIZE }, () =>
+        Array.from({ length: SIZE }, () => ({}))
+    );
+
+function GridManager() {
+
+}
---
+++ 사과쓰리매치서바이벌/object.js
@@ -0,0 +1,28 @@
+let number = [
+    { id: 1, img: "./images/number1.png" },
+    { id: 2, img: "./images/number2.png" },
+    { id: 3, img: "./images/number3.png" },
+    { id: 4, img: "./images/number4.png" },
+    { id: 5, img: "./images/number5.png" },
+    { id: 6, img: "./images/number6.png" },
+    { id: 7, img: "./images/number7.png" },
+    { id: 8, img: "./images/number8.png" },
+    { id: 9, img: "./images/number9.png" },
+    { id: 0, img: "./images/number0.png" },
+]
+
+let type = [
+    { id: 1, img: "./images/green.png" },
+    { id: 2, img: "./images/heart.png" },
+    { id: 3, img: "./images/ice.png" },
+    { id: 4, img: "./images/purple.png" },
+    { id: 5, img: "./images/star.png" }
+]
+
+function Object(scoreId, typeId) {
+    this.scoreId = scoreId;
+    this.typeId = typeId;
+}
+
+
+
